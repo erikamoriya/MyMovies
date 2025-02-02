f881663b59461ae6e8a74c8e0b659d770cb12a2b
moriya <erikamoriya@gmail.com>
2025-02-02 11:14:23 -0300

    initial commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Érika Moriya on 02/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
